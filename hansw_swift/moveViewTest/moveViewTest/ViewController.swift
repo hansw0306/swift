178ce793a97d860c853ed36b536eea70921ee7cf
hansw <hansw@thinkm.co.kr>
2020-05-28 14:10:14 +0900

    프로젝트 커밋

---
+++ hansw_swift/moveViewTest/moveViewTest/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  moveViewTest
+//
+//  Created by SANGWON HAN on 2020/05/28.
+//  Copyright © 2020 SANGWON HAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+        
+        //버튼 관련 작업
+        //테두리 그리기
+        //addClothesButton.layer.borderColor = UIColor.white.cgColor
+        //addClothesButton.layer.borderWidth = 2
+        
+        //버튼의 모서리 부분을 둥글게 처리
+        //addClothesButton.layer.cornerRadius = 10
+
+        
+    }
+    
+
+
+}
+
