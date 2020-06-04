edc56c9645816e25e38e64fa3c3c845bcdaa2251
hansw <hansw@thinkm.co.kr>
2020-06-04 17:25:01 +0900

    로그인 화면

--- hansw_swift/moveViewTest/moveViewTest/ViewController.swift
+++ hansw_swift/moveViewTest/moveViewTest/ViewController.swift
@@ -10,21 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //이메일 주소
+    @IBOutlet var idLable: UITextField!
+    //비밀번호
+    @IBOutlet var passWordLable: UITextField!
+    //비밀번호 보기 버튼
+    @IBOutlet var passWordLookButton: UIButton!
+    //자동로그인 버튼
+    @IBOutlet var autoLoginButton: UIButton!
+    //로그인 버튼
     @IBOutlet var loginButton: UIButton!
+    //계정생성 버튼
+    @IBOutlet var accountCreationButton: UIButton!
+
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+//---------------객체 초기 설정-----------------
+
+//1. 입력창 설정
+        //비밀번호는 안보이도록 가려주도록 하자.
+        passWordLable.isSecureTextEntry = true;
+        
+//2. 버튼 설정
+        //테두리 그리기
+        loginButton.layer.borderColor = UIColor.blue.cgColor
+        loginButton.layer.borderWidth = 2
+        accountCreationButton.layer.borderColor = UIColor.blue.cgColor
+        accountCreationButton.layer.borderWidth = 2
+        
+        
+        //버튼의 모서리 부분을 둥글게 처리
+        loginButton.layer.cornerRadius = 10
+        accountCreationButton.layer.cornerRadius = 10
+        
+//-------------------------------------------
         
         //chevron.down.square
         //버튼 관련 작업
-        //테두리 그리기
-        //addClothesButton.layer.borderColor = UIColor.white.cgColor
-        //addClothesButton.layer.borderWidth = 2
-        
-        //버튼의 모서리 부분을 둥글게 처리
-        //addClothesButton.layer.cornerRadius = 10
+
 
         
     }
@@ -52,9 +79,18 @@ class ViewController: UIViewController {
                }
     }
     
+    @IBAction func passWordLookButtonActopm(_ sender: Any) {
+
+        
+        //chevron.down.square.fill
+    }
     
-    //chevron.down.square.fill
     
+    
+    @IBAction func autoLoginButtonAction(_ sender: Any) {
+        
+        
+    }
     
     
 // MARK: -
