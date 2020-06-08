164a683868ef1eae996c370a15ba01fb76095c44
hansw <hansw@thinkm.co.kr>
2020-06-08 15:37:34 +0900

    메인화면 navigationItem 작업

--- hansw_swift/moveViewTest/moveViewTest/Source/Views/Main/MainViewController.swift
+++ hansw_swift/moveViewTest/moveViewTest/Source/Views/Main/MainViewController.swift
@@ -15,8 +15,25 @@ class MainViewController: UIViewController {
         
         title = "메인"
         
+        if #available(iOS 13.0, *) {
+            navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage.init(systemName: "person.fill"), style: .plain, target: self, action: #selector(NavigationItemButton))
+            navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Right", style: .plain, target: self, action: #selector(NavigationItemButton))
+            
+            
+        } else {
+            // Fallback on earlier versions
+        }
+        
         //아래 링크의 이미지를 사용하여 화면을 꾸며주는 작업을 하도록 하지.
         //https://www.youtube.com/watch?v=k90V115zqRk
+        
+        //이부분도 나중에 추가 작업 진행해 보자
+        //https://eastroot1590.tistory.com/entry/UICollectionView-3-실습-CardNews1?category=848449
+        
+        //요기 좋다
+        //https://unclean.tistory.com/60?category=834416
+        
+        
         
         
         // Do any additional setup after loading the view.
@@ -31,4 +48,13 @@ class MainViewController: UIViewController {
 //        super.viewWillDisappear(animated)
 //        navigationController?.hidesBarsOnTap = false
 //    }
+    
+    
+    @objc func NavigationItemButton() {
+        
+        print("네비게이션 버튼 클릭됨")
+        
+    }
+    
+    
 }
