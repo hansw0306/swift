0a7ca8022a6f25c6c19e203e7a82412f47aa17f4
hansw <hansw@thinkm.co.kr>
2020-06-29 15:24:05 +0900

    탈옥체크 로직 추가

--- hansw_swift/moveViewTest/moveViewTest/Source/Library/NativeFunc.swift
+++ hansw_swift/moveViewTest/moveViewTest/Source/Library/NativeFunc.swift
@@ -15,6 +15,10 @@ import LocalAuthentication
 
 class NativeFuc: NSObject, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
     
+    typealias MobileLibraryResult = (NSObject?) -> Void
+    
+    
+
     //MARK:- 화면관련
      
     //MARK:0. 최상위 viewController
@@ -198,6 +202,39 @@ class NativeFuc: NSObject, UIImagePickerControllerDelegate & UINavigationControl
     func removeValue(key:String) {
         myUserDefaults.removeObject(forKey: key)
     }
+    
+    
+    //MARK:2. 탈옥체크
+    //결과가 true이면 탈옥된 디바이스 이다.
+     func getJailbrokenStatus() -> Bool {
+        if TARGET_IPHONE_SIMULATOR != 1 {
+            // Check 1 : existence of files that are common for jailbroken devices
+            if FileManager.default.fileExists(atPath: "/Applications/Cydia.app")
+                || FileManager.default.fileExists(atPath: "/Library/MobileSubstrate/MobileSubstrate.dylib")
+                || FileManager.default.fileExists(atPath: "/bin/bash")
+                || FileManager.default.fileExists(atPath: "/Applications/Icy.app")
+                || FileManager.default.fileExists(atPath: "/usr/sbin/sshd")
+                || FileManager.default.fileExists(atPath: "/etc/apt")
+                || FileManager.default.fileExists(atPath: "/private/var/lib/apt/")
+                || UIApplication.shared.canOpenURL(URL(string:"cydia://package/com.example.package")!) {
+                return true
+            }
+            // Check 2 : Reading and writing in system directories (sandbox violation)
+            let stringToWrite = "Jailbreak Test"
+            do {
+                try stringToWrite.write(toFile:"/private/JailbreakTest.txt", atomically:true, encoding:String.Encoding.utf8)
+                //Device is jailbroken
+                return true
+            } catch {
+                return false
+            }
+        }
+        else {
+            return false
+        }
+    }
+    
+    
     
     
     //MARK:10. 비동기 처리
