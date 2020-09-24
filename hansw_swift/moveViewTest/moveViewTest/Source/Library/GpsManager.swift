e34f1f11de9d89b3131d57ff9357a2620cd07124
hansw <hansw@thinkm.co.kr>
2020-09-24 11:17:01 +0900

    정리

--- hansw_swift/moveViewTest/moveViewTest/Source/Library/GpsManager.swift
+++ hansw_swift/moveViewTest/moveViewTest/Source/Library/GpsManager.swift
@@ -43,41 +43,39 @@ class GpsManager: NSObject, CLLocationManagerDelegate {
         return "locations = \(locValue.latitude) \(locValue.longitude)"
     }
 
-    //위치정보를 지속적으로 확인하는 방법 (비콘)
+    //지속적으로 (비콘)위치정보를 확인하는 방법
+    func startScanning() {
+        
+        let uuid = UUID(uuidString: "5A4BCFCE-174E-4BAC-A814-092E77F6B7E5")!
+        let beaconRegion = CLBeaconRegion(proximityUUID: uuid, major: 123, minor: 456, identifier: "MyBeacon")
+        
+        locationManager.startMonitoring(for: beaconRegion)
+        locationManager.startRangingBeacons(in: beaconRegion)
+    }
     
-        func startScanning() {
-            
-            
-            let uuid = UUID(uuidString: "5A4BCFCE-174E-4BAC-A814-092E77F6B7E5")!
-            let beaconRegion = CLBeaconRegion(proximityUUID: uuid, major: 123, minor: 456, identifier: "MyBeacon")
-    
-            locationManager.startMonitoring(for: beaconRegion)
-            locationManager.startRangingBeacons(in: beaconRegion)
-        }
-
-        func update(distance: CLProximity) {
-            UIView.animate(withDuration: 0.8) { [unowned self] in
-                switch distance {
-                case .unknown:
-                    print("UNKNOWN")
-                case .far:
-                    print("FAR")
-                case .near:
-                    print("NEAR")
-                case .immediate:
-                    print("RIGHT HERE")
-                }
+    func update(distance: CLProximity) {
+        UIView.animate(withDuration: 0.8) { [unowned self] in
+            switch distance {
+            case .unknown:
+                print("UNKNOWN")
+            case .far:
+                print("FAR")
+            case .near:
+                print("NEAR")
+            case .immediate:
+                print("RIGHT HERE")
             }
         }
-
-        func locationManager(_ manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion) {
-            if beacons.count > 0 {
-                let beacon = beacons[0]
-                update(distance: beacon.proximity)
-            } else {
-                update(distance: .unknown)
-            }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion) {
+        if beacons.count > 0 {
+            let beacon = beacons[0]
+            update(distance: beacon.proximity)
+        } else {
+            update(distance: .unknown)
         }
+    }
     
     
 }
