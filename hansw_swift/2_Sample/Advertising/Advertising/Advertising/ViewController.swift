6f76b397dafbbd4f02b2ae05573514b29c6848d3
hansw <hansw@thinkm.co.kr>
2020-10-14 13:20:10 +0900

    //$$ TEST---> 인디게이터를 만들고 아래의 reloadData를 했을때 지우는 작업을 넣어주도록 한다.

--- hansw_swift/2_Sample/Advertising/Advertising/Advertising/ViewController.swift
+++ hansw_swift/2_Sample/Advertising/Advertising/Advertising/ViewController.swift
@@ -15,16 +15,17 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+//MARK: 데이터 작업
         mCommunicationAPI = CommunicationAPI.init()
-        
         mCommunicationAPI.httpJson(pageInt:"5") { (result, error) in
             if let result = result {
                 print("success: \(result)")
                 
+//                DispatchQueue.main.async {
+//
+//                }
+                self.mainTableView.setJsonData(jsonData: result)
                 DispatchQueue.main.async {
-                    
-                    self.mainTableView.setJsonData(jsonData: result)
-                    
                     self.mainTableView.dataSource = self.mainTableView
                     self.mainTableView.delegate = self.mainTableView
                     //스토리보드에 같이 있을때
@@ -32,15 +33,14 @@ class ViewController: UIViewController {
                     
                     let viewCellNibName = UINib(nibName: "MainTableViewCell", bundle: nil)
                     self.mainTableView.register(viewCellNibName, forCellReuseIdentifier: "MainTableViewCell")
+                    
+                    //$$ TEST---> 인디게이터를 만들고 아래의 reloadData를 했을때 지우는 작업을 넣어주도록 한다.
+                    self.mainTableView.reloadData()
                 }
             } else if let error = error {
                 print("error: \(error.localizedDescription)")
             }
         }
-        
-        
-        
-        
         
     }
 
