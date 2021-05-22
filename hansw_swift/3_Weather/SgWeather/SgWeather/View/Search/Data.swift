aa262b3c7f6c2fdce0957dafdc6898d46f293c10
hansw <hansw@thinkm.co.kr>
2021-05-22 20:43:23 +0900

    검색 화면 추가

---
+++ hansw_swift/3_Weather/SgWeather/SgWeather/View/Search/Data.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+class Data{
+    var main:String
+    var detail:detailtype
+    
+    
+    init(main: String, detail: detailtype) {
+        self.main = main
+        self.detail = detail
+    }
+}
+
+enum detailtype:String {
+    case A = "A"
+    case B = "B"
+}
---
+++ hansw_swift/3_Weather/SgWeather/SgWeather/View/Search/TableViewController.swift
@@ -0,0 +1,66 @@
+//
+//  TableViewController.swift
+//  search_extension
+//
+//  Created by 송동욱 on 26/09/2018.
+//  Copyright © 2018 송동욱. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController , UISearchControllerDelegate, UISearchResultsUpdating, UISearchBarDelegate{
+   
+    //create the search controller and result contoller
+    var dataArray = [Data]()
+    var fileteredData = [Data]()
+    
+    var searchController = UISearchController()
+    var resultVC = UITableViewController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setData()
+        searchController = UISearchController(searchResultsController: resultVC)
+        
+        tableView.tableHeaderView = searchController.searchBar
+        //usally good to set the presentation context  
+        self.definesPresentationContext = true
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.delegate = self
+        searchController.delegate = self
+        
+        resultVC.tableView.delegate = self
+        resultVC.tableView.dataSource = self
+    }
+
+    func updateSearchResults(for searchController: UISearchController) {
+        fileteredData = dataArray.filter({ (data:Data) -> Bool in
+            return data.main.lowercased().contains(searchController.searchBar.text!.lowercased())
+        })
+        resultVC.tableView.reloadData()
+    }
+    
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableView == resultVC.tableView ? fileteredData.count : dataArray.count
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = (tableView == resultVC.tableView ? fileteredData[indexPath.row].main : dataArray[indexPath.row].main)
+        return cell
+    }
+    
+    private func setData(){
+        dataArray.append(Data(main: "One", detail: .A))
+        dataArray.append(Data(main: "Two", detail: .A))
+        dataArray.append(Data(main: "Three", detail: .A))
+        dataArray.append(Data(main: "Ten", detail: .B))
+        dataArray.append(Data(main: "Eleven", detail: .B))
+        dataArray.append(Data(main: "Twelve", detail: .B))
+    }
+
+}
+
