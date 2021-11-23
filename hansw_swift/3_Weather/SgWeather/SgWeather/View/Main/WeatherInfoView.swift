607c4f98df69296cc1f7acc3cdd05ae1582a774d
SANGWON HAN <>
2021-11-23 21:47:09 +0900

    정리

--- hansw_swift/3_Weather/SgWeather/SgWeather/View/Main/WeatherInfoView.swift
+++ hansw_swift/3_Weather/SgWeather/SgWeather/View/Main/WeatherInfoView.swift
@@ -9,6 +9,15 @@ import UIKit
 
 class WeatherInfoView: UIView {
 
+    //화면
+    @IBOutlet var mainInfoView: UIView!
+    @IBOutlet var mainImageView: UIImageView!
+    @IBOutlet var placeLable: UILabel!
+    @IBOutlet var placeWeatherLable: UILabel!
+    @IBOutlet var placeTemperaturesLable: UILabel!
+    @IBOutlet var humidityLable: UILabel!
+    
+    
     /*
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
